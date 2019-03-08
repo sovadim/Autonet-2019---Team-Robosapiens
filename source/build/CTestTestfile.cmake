0073e749c9089e4fb8774d6400755d21b27ad202
Shauberg <Shauberg@mail.ru>
2019-03-08 20:13:01 +0300

    0

---
+++ source/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/vadim/Projects/Autonet-2019---Team-Robosapiens/source/src
+# Build directory: /home/vadim/Projects/Autonet-2019---Team-Robosapiens/source/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(Control)
